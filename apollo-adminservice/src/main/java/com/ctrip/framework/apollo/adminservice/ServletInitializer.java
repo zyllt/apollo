a71cf28fa272dd69e2e9820494da51fcb1208c1c
zengyuan <zengyuan9101@gmail.com>
2018-05-11 15:51:40 +0800

    增加webapp启动入口

---
+++ apollo-adminservice/src/main/java/com/ctrip/framework/apollo/adminservice/ServletInitializer.java
@@ -0,0 +1,19 @@
+package com.ctrip.framework.apollo.adminservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * 功能：web
+ *
+ * @author zengyuan on 2018/5/11.
+ * @see
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(AdminServiceApplication.class);
+  }
+
+}
